3190e2752effbbaab4f6740b63ded83426c1057d
Artur <artur.aleksanyan89@gmail.com>
2013-07-13 14:52:23 +0400

    few changes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-defined('PUBLIC_DIR') or define('PUBLIC_DIR', dirname(__FILE__));
+//defined('PUBLIC_DIR') or define('PUBLIC_DIR', dirname(__FILE__));
 define('OFFSET_UPPER_BOUND', 30);
 
 require_once(/*PUBLIC_DIR.*/"./lib/Get_Rhymebrain.php");
@@ -13,7 +13,7 @@ $errors = 0;
 while($offset < OFFSET_UPPER_BOUND){
     $arr_res =  $words->getWord($offset,10);
     handleWords($arr_res);
-    echo "while($offset < OFFSET_UPPER_BOUND)   " . $offset;
+    echo "while($offset < OFFSET_UPPER_BOUND) " . $offset . "\r\n";
 }
 
 function handleWords($arr_res){
@@ -42,9 +42,12 @@ function handleWords($arr_res){
 
 function incrementOffset(){
     global $offset;
-    $offset = file_get_contents("./lib/offset");
-    file_put_contents("./lib/offset", ++$offset);
-//    echo "offset is : " . file_get_contents("./lib/offset");
+    $offset = intval(file_get_contents("./lib/offset"));
+//    echo "offset : before " . $offset . "\r\n";
+    $offset = $offset + 1;
+//    echo "offset : after " . $offset . "\r\n";
+    file_put_contents("./lib/offset", $offset) . "\r\n";
+    echo "offset written : " . file_get_contents("./lib/offset") . "\r\n";
 }
 
 function startsWith($haystack, $needle = "[")
