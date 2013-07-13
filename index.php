819c996156c323d02490925adb7ea0e0d1b39afc
Artur <artur.aleksanyan89@gmail.com>
2013-07-13 12:23:22 +0400

    some typo fixed. Saving offset in the file + resuming capability. Stable version

--- index.php
+++ index.php
@@ -1,32 +1,53 @@
 <?php
 defined('PUBLIC_DIR') or define('PUBLIC_DIR', dirname(__FILE__));
+define('OFFSET_UPPER_BOUND', 30);
 
 require_once(/*PUBLIC_DIR.*/"./lib/Get_Rhymebrain.php");
 require_once (/*PUBLIC_DIR.*/"./lib/Word_Table.php");
 
 $rhyme = new Get_Rhymebrain();
 $words = new Word_Table();
-$arr_res =  $words->getWord(0,500);
-$ofset = 0;
+$offset = file_get_contents("./lib/offset");
 $errors = 0;
-foreach($arr_res as $word)
-{
-    $responce = $rhyme->getRhymes($word);
-    if(startsWith($responce)){
-        $words->insertRhyme($word,$responce);
-        $ofset++;
-        echo $ofset  . substr($responce, 0, 20) ."\r\n";
-    } else {
-        echo "error " . ++$errors . substr($responce, 0, 20) . "\r\n";
-//        echo $responce; exit;
-        sleep(1);
-        continue;
-    }
 
+while($offset < OFFSET_UPPER_BOUND){
+    $arr_res =  $words->getWord($offset,10);
+    handleWords($arr_res);
+    echo "while($offset < OFFSET_UPPER_BOUND)   " . $offset;
+}
+
+function handleWords($arr_res){
+    global $rhyme;
+    global $words;
+    global $offset;
+    global $errors;
+
+    foreach($arr_res as $word)
+    {
+        $response = $rhyme->getRhymes($word);
+
+        // while the response is an error, keep sending the request with the same word
+        while(!startsWith($response)){
+            echo "error " . "WORD : " . $word . " " . ++$errors . " " . substr($response, 0, 20) . "\r\n";
+//        echo $response; exit;
+            sleep(1);
+            $response = $rhyme->getRhymes($word);
+        }
+        $words->insertRhyme($word,$response);
+        echo $offset . " WORD : " . $word . substr($response, 0, 20) ."\r\n";
+        incrementOffset();
+    }
+}
+
+
+function incrementOffset(){
+    global $offset;
+    $offset = file_get_contents("./lib/offset");
+    file_put_contents("./lib/offset", ++$offset);
+//    echo "offset is : " . file_get_contents("./lib/offset");
 }
 
 function startsWith($haystack, $needle = "[")
 {
     return !strncmp($haystack, $needle, strlen($needle));
 }
-?>
